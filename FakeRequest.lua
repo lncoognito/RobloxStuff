b6dbc8b7d57a957d1d099965c1a5e0a0d2e9967f
Incognito <75094867+lncoognito@users.noreply.github.com>
2022-08-03 02:48:54 +0300

    Update FakeRequest.lua

--- FakeRequest.lua
+++ FakeRequest.lua
@@ -1,1 +1,28 @@
-.
+function FakeRequest(Username, PersonPicture)
+    local BindableFunction  = Instance.new("BindableFunction", game.Workspace)
+
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title           = Username,
+        Text            = "Sent you a friend request!",
+        Icon            = "rbxassetid://"..PersonPicture,
+        Duration        = 5,
+        Callback        = BindableFunction,
+        Button1         = "Accept",
+        Button2         = "Decline"
+    })
+
+    BindableFunction.OnInvoke = function(Response)
+        if Response == "Accept" then
+            game:GetService("StarterGui"):SetCore("SendNotification", {
+                Title       = "New Friend",
+                Text        = Username,
+                Icon        = "rbxassetid://"..PersonPicture,
+                Duration    = 5
+            }) 
+
+            BindableFunction:Destroy()
+        end
+    end 
+end
+
+FakeRequest("Fake", "2342343")
